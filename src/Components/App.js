aa74e9e21ac67e1b025770f08140589c52d586d1
durveshramane26 <durveshramane6@gmail.com>
2022-08-04 17:48:49 +0530

    Stored data into localStorage

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import { uuid } from "uuidv4";
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) => {
-    console.log(contact);
-    setContacts([...contacts, contact]);
+    console.log(contact);  
+    setContacts([...contacts, { id: uuid(), ...contacts }]);
   }
-  return (
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts])
+  return (  
     <div className="ui container">
       <Header />
       <AddContact  addContactHandler={addContactHandler}/>
